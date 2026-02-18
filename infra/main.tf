631fc88c55da91f2e6ca651243af8bcb8ac4be02
ryanstanley4 <ryan@stanley4.info>
2026-02-18 19:03:43 +0000

    Update VM module configurations for control and worker nodes

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,20 @@
 module "control_vms" {
   source       = "./modules/proxmox-vm"
   name_prefix  = "k3s-control-"
-  vm_count     = 1
-  target_nodes = ["suzuka", "monaco", "monza"]
+  vm_count     = 3
+  target_nodes = ["monaco","suzuka", "monza"]
   storage      = "k3s"
   tags         = ["k3s_cluster","terraform", "control_plane"]
-  clone_from   = "k3s-template-16-02-26"
+  clone_from   = "106"
+}
+
+
+module "worker_vms" {
+  source       = "./modules/proxmox-vm"
+  name_prefix  = "k3s-worker-"
+  vm_count     = 3
+  target_nodes = ["monaco","suzuka", "monza"]
+  storage      = "k3s"
+  tags         = ["k3s_cluster","terraform", "worker_node"]
+  clone_from   = "106"
 }
